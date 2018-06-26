ffc5a8c73f14d92fd1c8de391a846d331ab91e37
Hao Luo <howlowck@gmail.com>
2018-06-26 16:31:47 -0500

    init genTemp

---
+++ generatorTemplate.js
@@ -0,0 +1,25 @@
+/** generator template file
+ * parameters: none
+ * returns: {
+ *   name,
+ *   description,
+ *   image,
+ *   function definition
+ * }
+ */
+
+(function (window) {
+  const generatorObj = {
+    name: 'Sine Wave Generate',
+    description: 'Sine Wave Generator outputs the value in a sine wave pattern given the timestamp',
+    image: '',
+    generatorFunction: (minAmp, maxAmp, period) => (timestamp) => {
+      // convert timestamp into ms, int value
+      // (timestamp_int % (pi * 2)) - pi
+      // -pi < result < pi
+
+      return 1
+    }
+  }
+  return generatorObj
+})(window)
