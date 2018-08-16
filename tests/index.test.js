e3c3ed0d905c85c4478f8600754de5f4b58ede48
Gabrielle Crevecoeur <gacreve@microsoft.com>
2018-08-16 13:21:31 -0500

    jest testing updated

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,24 +1,35 @@
+/* global expect test */
 const _g_ = require('../index')
 
-let sineGenerator = _g_('sine')
+test('access sine generator', () => {
+  let sineGenerator = _g_('sine')
+  expect(typeof sineGenerator).toEqual('function')
 
-console.log(sineGenerator)
+  // console.log(sineGenerator)
+})
 
-_g_.fn.sawtooth = {
-  name: '{Descriptive Name}',
-  description: '{Generator Description}',
-  image: '{Image link}',
-  functionName: 'sawtooth',
-  functionArgs: ['Paramater argument names; array of string'],
-  generatorFunction: (functionParamters) => (timestamp) => {}
-}
+test('add new generator and access it', () => {
+  _g_.fn.sawtooth = {
+    name: '{Descriptive Name}',
+    description: '{Generator Description}',
+    image: '{Image link}',
+    functionName: 'sawtooth',
+    functionArgs: ['Paramater argument names; array of string'],
+    generatorFunction: (functionParamters) => (timestamp) => {}
+  }
+  expect(_g_.fn).toHaveProperty('sawtooth')
 
-let sawtoothGenerator = _g_('sawtooth')
+  let sawtoothGenerator = _g_('sawtooth')
+  expect(typeof sawtoothGenerator).toEqual('function')
+})
 
-console.log(sawtoothGenerator)
+test('attempt to access generator but misspelled', () => {
+  let sawtoothGenerator = _g_('sawtoth')
+  expect(sawtoothGenerator).toBe(null)
+})
 
-let generatorsArray = _g_.allGenerators()
-
-console.log(generatorsArray)
-
-// look into jest
+test('return all generators', () => {
+  let generatorsArray = _g_.allGenerators()
+  expect(generatorsArray).toHaveProperty('sine')
+  expect(generatorsArray).toHaveProperty('sawtooth')
+})
